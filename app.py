906a2cf6514344fc1c46cf1576dd4b86fea68c46
Rori Macaulay Cooper <rori.m.cooper@gmail.com>
2018-09-22 20:10:12 -0500

    b-u 2

--- app.py
+++ app.py
@@ -1,30 +1,44 @@
+# import necessary libraries
 from flask import Flask, render_template, jsonify, redirect
 import pymongo
 from flask_pymongo import PyMongo
 import scrape_mars
 
+# create instance of Flask app
 app = Flask(__name__)
+
+# Use flask_pymongo to set up mongo connection
 app.config["MONGO_URI"] = "mongodb://localhost:27017/scrape"
 mongo = PyMongo(app)
 
+# create route that renders index.html template and finds documents from mongo
 @app.route("/")
 def index():
+    # Find data using try/except - kept getting failures
     try:
         mars_data = mongo.db.mars_data.find_one()
         return render_template('index.html', mars_data=mars_data)
     except:
+        # return template and data
         return redirect("http://localhost:5000/scrape", code=302)
 
+# Route that will trigger scrape functions
 @app.route("/scrape")
 def scraped():
+    # Run scraped functions
     mars_data = mongo.db.mars_data
     mars_data_scrape = scrape_mars.scrape()
+
+    # Update Mars scrape data into database
     mars_data.update(
         {},
         mars_data_scrape,
         upsert=True
     )
+    # Redirect back to home page
     return redirect("http://localhost:5000/", code=302)
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+    # Notes - used Week 3 instructor 'scrape_and_render' and student 'scrape_weather' activities plus other references found on GitHub to get this code to execute properly.
--- scrape_mars.py
+++ scrape_mars.py
@@ -7,23 +7,26 @@ from selenium import webdriver
 import requests
 import time
 
+# Initialize browser
 def init_browser():
-    """ Connects path to chromedriver """
+    # Connects path to chromedriver
 
     executable_path = {'executable_path': 'chromedriver.exe'}
     return Browser("chrome", **executable_path, headless=True)
 
+# Function to scrape for all required Mars data
 def scrape():
     """ Scrapes all websites for Mars data """
 
-    # Create a python dictionary to store all data
+    # Begin dictionary to store data
     scrape_mars_dict = {}
 
-    # Use requests and BeautifulSoup to scrape Nasa News for latest news
+    # Visit Nasa.gov and use BeautifulSoup to scrape latest news title and summarydescription
     url = 'https://mars.nasa.gov/news/?page=0&per_page=40&order=publish_date+desc%2Ccreated_at+desc&search=&category=19%2C165%2C184%2C204&blank_scope=Latest'
     response = requests.get(url)
     soup = bs(response.text, 'lxml')
 
+    # Find latest news
     results = soup.find('div', class_='features')
     news_title = results.find('div', class_='content_title').text
     newsp = results.find('div', class_='rollover_description').text
@@ -32,18 +35,19 @@ def scrape():
     scrape_mars_dict['news_title'] = news_title
     scrape_mars_dict['newsp'] = newsp
 
-    # Scrape Mars Weather twitter for latest weather report
+    # Visit twitter and scrape latest weather report
     twitter_url = 'https://twitter.com/marswxreport?lang=en'
     twitter_response = requests.get(twitter_url)
     twitter_soup = bs(twitter_response.text, 'lxml')
 
+    # Find weather data
     twitter_result = twitter_soup.find('div', class_='js-tweet-text-container')
     mars_weather = twitter_result.find('p', class_='js-tweet-text').text
 
     # Store scraped data into dictionary
     scrape_mars_dict['mars_weather'] = mars_weather
 
-    # Scrape facts about Mars from space-facts.com using Pandas read_html function
+    # Visit space-facts.com and scrape facts using Pandas read_html function
     mars_facts_url = 'https://space-facts.com/mars/'
     tables = pd.read_html(mars_facts_url)
     df = tables[0]
@@ -55,76 +59,82 @@ def scrape():
     mars_facts.replace("\n","")
     df.to_html('mars_facts.html')
 
-    # Store html file to dictionary
+    # Store scraped data to dictionary
     scrape_mars_dict['mars_facts'] = mars_facts
 
     # Call on chromedriver function to use for splinter
     browser = init_browser()
 
 
-
-    # Scrape Nasa for url of latest featured image of Mars
+    # Vist Nasa and scrape latest featured image of Mars
     nasa_url = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
     browser.visit(nasa_url)
 
+    # Use Beautiful Soup
     nasa_html = browser.html
     nasa_soup = bs(nasa_html, "lxml")
 
+    # Retrieve URL
     featured_image = nasa_soup.find('div', class_='default floating_text_area ms-layer').find('footer')
     featured_image_url = 'https://www.jpl.nasa.gov'+ featured_image.find('a')['data-fancybox-href']
 
-    # Store url to dictionary
+    # Store img_url to dictionary
     scrape_mars_dict['featured_image_url'] = featured_image_url
 
-    # Scrape astrogeology.usgs.gov for urls of hemisphere images of Mars
+    # Visit astrogeology.usgs.gov and scrape img urls of Mars' hemispheres
     hemisphere_url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
     browser.visit(hemisphere_url)
 
+    # Use Beautiful Soup
     hemisphere_html = browser.html
     hemisphere_soup = bs(hemisphere_html, 'lxml')
     base_url ="https://astrogeology.usgs.gov"
 
+    # Retrieve URLs
     image_list = hemisphere_soup.find_all('div', class_='item')
 
-    # Create a list to store dictionary of urls and image titles
+    # Container for images
     hemisphere_image_urls = []
 
-    # Loop through list of hemispheres and click on each one to find large resolution image
+    # Loop through list of hemispheres images
     for image in image_list:
 
-        # Create a dicitonary to store urls and titles
+        # Store urls and titles in dictionary
         hemisphere_dict = {}
 
-        # Find link to large image
+        # Find large images
         href = image.find('a', class_='itemLink product-item')
         link = base_url + href['href']
 
-        # Visit the link
+        # Visit URL
         browser.visit(link)
 
-        # Wait 1 second
+        # Halt for time
         time.sleep(1)
 
-        # Parse the html of the new page
+        # Use Beautiful Soup
         hemisphere_html2 = browser.html
         hemisphere_soup2 = bs(hemisphere_html2, 'lxml')
 
-        # Find the title
+        # Find the img-title
         img_title = hemisphere_soup2.find('div', class_='content').find('h2', class_='title').text
 
-        # Append to dict
+        # Update dictionary
         hemisphere_dict['title'] = img_title
 
         # Find image url
         img_url = hemisphere_soup2.find('div', class_='downloads').find('a')['href']
 
-        # Append to dict
+        # Update dictionary
         hemisphere_dict['url_img'] = img_url
 
-        # Append dict to list
+        # Update container
         hemisphere_image_urls.append(hemisphere_dict)
 
-    # Store hemisphere image urls to dictionary
+    # Store img data to dictionary
     scrape_mars_dict['hemisphere_image_urls'] = hemisphere_image_urls
 
+    # Return results
     return scrape_mars_dict
+
+    # Notes - used Week 3 instructor 'scrape_and_render' and student 'scrape_weather' activities plus other references found on GitHub to get this code to execute properly.
